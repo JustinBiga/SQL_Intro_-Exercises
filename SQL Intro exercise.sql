20c4fbfbc96b477ebd2c6c5f8790c0bfa53281f6
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-19 10:31:48 -0400

    Init commit

--- SQL Intro exercise.sql
+++ SQL Intro exercise.sql
@@ -5,26 +5,25 @@ SELECT * FROM Products
 WHERE Price = 1400;
 -- find all products that cost $11.99 or $13.99
 SELECT * FROM Products
-WHERE Price = 11.99 OR 13.99;
+WHERE Price = 11.99 OR Price = 13.99;
 -- find all products that do NOT cost 11.99 - using NOT
 SELECT * FROM Products WHERE NOT Price =11.99;
 -- find all products and sort them by price from greatest to least
 SELECT * FROM Products ORDER BY price DESC;
 -- find all employees who don't have a middle initial
-SELECT * FROM employees WHERE MiddleInitial = null;
+SELECT * FROM employees WHERE MiddleInitial IS null;
 -- find distinct product prices
-SELECT DISTINCT Prices FROM Products;
+SELECT DISTINCT Price FROM Products;
 -- find all employees whose first name starts with the letter ‘j’
-SELECT * FROM Employees WHERE FirstName = 'j';
+SELECT * FROM Employees WHERE FirstName LIKE 'j%';
 -- find all Macbooks
-SELECT * FROM Products ORDER By Name;
-SELECT * FROM products WHERE Name LIKE 'Macbook';
+SELECT * FROM Products WHERE Name LIKE '%Macbook%';
 -- find all products that are on sale
-SELECT * FROM Products WHERE onsale;
+SELECT * FROM Products WHERE onsale = true;
 -- find the average price of all products
-SELECT AVG(Price) "Average of all Products" FROM Products;
+SELECT AVG(Price) FROM Products;
 -- find all Geek Squad employees who don't have a middle initial
 SELECT * FROM Employees WHERE Title = "Geek Squad" AND MiddleInitial IS Null;
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT * FROM Products WHERE Stocklevel BETWEEN 500 AND 1200
-ORDER BY price;
+ORDER BY price ASC;
